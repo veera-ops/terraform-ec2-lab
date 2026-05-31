965d7ba6f2d85e4bffc6c20759b81218873d624e
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-05-31 22:16:58 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -36,7 +36,6 @@ resource "aws_instance" "server" {
   instance_type               = "t2.micro"
 
   subnet_id                   = aws_subnet.public.id
-  key_name                    = aws_key_pair.key.key_name
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
 
