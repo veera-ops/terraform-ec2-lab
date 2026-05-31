7279c73bcede9ca91120ee4ab68c65d266e2280c
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-05-31 23:01:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,9 @@
 output "public_ip" {
   value = aws_instance.server.public_ip
 }
+output "tgw_id" {
+  value = aws_ec2_transit_gateway.tgw.id
+}
 output "dxgw_id" {
   value = aws_dx_gateway.dxgw.id
 }
